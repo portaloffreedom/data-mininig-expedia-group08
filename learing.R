32546892b203492fbb6c84d35f29bc10649553e8
Matteo De Carlo <matteo.dek@gmail.com>
2015-05-11 18:48:25 +0200

    BUGFIX and mlp prototype

--- learing.R
+++ learing.R
@@ -29,7 +29,7 @@ calculate_error_for_id <- function(srch_id, booked_prob) {
     return(errors)
 }
 
-analyze <- function(data, train, fwd) {
+analyze <- function(data, train_fn, fwd_fn) {
   
   cross_sections <- 10
   unique_ids <- unique(data$srch_id)
@@ -45,7 +45,7 @@ analyze <- function(data, train, fwd) {
     selection <- crossSelection == i
     train_ids <- unique_ids[!selection]
     #test_ids <- unique_ids[selection]
-    
+
     ## creating selection for data lines
     train_selection <- c()
     for (id in data$srch_id) {
@@ -53,27 +53,31 @@ analyze <- function(data, train, fwd) {
         #test_selection <- c(test_selection, id %in% test_ids)
     }
     
+    print("A")
     ## creating train and test dataset
     train <- data[train_selection,interesing_columns]
     test <- data[!train_selection,interesing_columns]
     
+    print("B")
     ## creating targets
     target_train <- train$booked_prob
     #target_test <- test$booked_prob
     
+    print("C")
     ## train
-    model <- train(train, target_train)
+    model <- train_fn(train, target_train)
     
+    print("D")
     ## results
-    result_train <- fwd(model, train)
-    result_test <- fwd(model, test)
+    result_train <- fwd_fn(model, train)
+    result_test <- fwd_fn(model, test)
     
+    print("E")
     ## calculate positions
     errors_train <- calculate_error_for_id(train$srch_id, result_train)
     errors_test <- calculate_error_for_id(test$srch_id, result_test)
     
-    ## calculate errors for id
-    
+    print("F")
     ## error calculation
     error_train[i] <- mean(errors_train)
     error_test[i] <- mean(errors_test)
@@ -83,4 +87,39 @@ analyze <- function(data, train, fwd) {
   print(error)
   
   return(error)
+}
+
+mlp_analyze <- function(data) {
+    library("RSNNS")
+    
+    errors<-c()
+    
+    forward <- function(model, data){
+        print("C1")
+        predict_data <- predict(model,data)
+        return(predict_data)
+    }
+
+    i_values <- 2:3
+    for (i in i_values) {
+        train <- function(train_data, target){
+            print(paste("C2",i))
+            print(train_data)
+            model <- mlp(x=train_data,                    #input data for training
+                         y=target,                        #output data (targets) for training
+                         size=i,                          #number of neurons in the hidden layer
+                         learnFunc="Std_Backpropagation", #type of learning
+                         learnFuncParams=c(0.01),         #paramenters of the learning function (eta)
+                         maxit=50000)                     #maximum number of iterations
+                                                        
+            return(model)
+        }
+        
+        error <- analyze(data, train, forward)
+        errors <- rbind(errors, error)
+    }
+    
+    plot(c(min(i_values),max(i_values)), c(min(errors), max(errors)) ,type="n")
+    lines(i_values, errors[,1], col="black")
+    lines(i_values, errors[,2], col="red")
 }
