5394ad2bc5e3e018accb17fd91d263a8c3d79e15
Matteo De Carlo <matteo.dek@gmail.com>
2015-05-14 16:09:17 +0200

    learning improved

--- learing.R
+++ learing.R
@@ -1,4 +1,6 @@
 N_CORES <- 2
+library(foreach)
+library(doParallel)
 
 
 ndcg <- function(x) {
@@ -16,14 +18,16 @@ summed_squared_difference <- function(result,target) {
   #return(mean(abs(difference)))
 }
 
-calculate_error_for_id <- function(srch_id, booked_prob) {
+calculate_error_for_id <- function(srch_id, booked_prob, real_booked_prob) {
     
     registerDoParallel(cores=N_CORES)
+    
     errors <- foreach(id = uinque_id_iter(unique(srch_id)), .combine='c') %dopar% {
         print(paste("now at",id))
         
-        prob_order <- order(booked_prob[srch_id == id], decreasing=F)
-        return(ndcg(prob_order));
+        real_prob_order <- order(real_booked_prob[srch_id == id], decreasing=T)
+        prob_order <- order(order(booked_prob[srch_id == id], decreasing=F))
+        return(1-ndcg(prob_order[real_prob_order]));
     }
     
     return(errors)
@@ -38,30 +42,40 @@ analyze <- function(data, train_fn, fwd_fn) {
   error_train <- numeric(cross_sections)
   error_test <- numeric(cross_sections)
   
-  interesing_columns <- c('site_id', 'prop_starrating', 'prop_location_score1', 'prop_review_score', 'prop_brand_bool')
+  interesing_columns <- c('site_id', 'prop_starrating', 'prop_location_score1', 'prop_review_score')
     
   for (i in 1:cross_sections) {
+    print("A0")
     ## creating selection for ids
     selection <- crossSelection == i
     train_ids <- unique_ids[!selection]
     #test_ids <- unique_ids[selection]
 
+    print("A1")
     ## creating selection for data lines
-    train_selection <- c()
-    for (id in data$srch_id) {
-        train_selection <- c(train_selection, id %in% train_ids)
-        #test_selection <- c(test_selection, id %in% test_ids)
+    registerDoParallel(cores=8)
+    train_selection <- foreach(id = uinque_id_iter(data$srch_id), .combine='c') %dopar% {
+        id %in% train_ids
     }
     
-    print("A")
+#     for (id in data$srch_id) {
+#         train_selection <- c(train_selection, id %in% train_ids)
+#         #test_selection <- c(test_selection, id %in% test_ids)
+#     }
+    
+    # test if the selected sets are empty
+    if (sum(train_selection) == 0) stop("train set empty")
+    if (sum(!train_selection) == 0) stop("test set empty")
+    
+    print("A2")
     ## creating train and test dataset
     train <- data[train_selection,interesing_columns]
     test <- data[!train_selection,interesing_columns]
     
     print("B")
     ## creating targets
-    target_train <- train$booked_prob
-    #target_test <- test$booked_prob
+    target_train <- data$booked_prob[train_selection]
+    #target_test <- data$booked_prob[!train_selection]
     
     print("C")
     ## train
@@ -74,8 +88,8 @@ analyze <- function(data, train_fn, fwd_fn) {
     
     print("E")
     ## calculate positions
-    errors_train <- calculate_error_for_id(train$srch_id, result_train)
-    errors_test <- calculate_error_for_id(test$srch_id, result_test)
+    errors_train <- calculate_error_for_id(data$srch_id[train_selection], result_train, data$booked_prob[train_selection])
+    errors_test <- calculate_error_for_id(data$srch_id[!train_selection], result_test, data$booked_prob[train_selection])
     
     print("F")
     ## error calculation
@@ -95,23 +109,23 @@ mlp_analyze <- function(data) {
     errors<-c()
     
     forward <- function(model, data){
-        print("C1")
+        print("D1")
         predict_data <- predict(model,data)
         return(predict_data)
     }
 
-    i_values <- 2:3
+    i_values <- 2:2
     for (i in i_values) {
         train <- function(train_data, target){
-            print(paste("C2",i))
-            print(train_data)
+            print(paste("C1",i))
             model <- mlp(x=train_data,                    #input data for training
                          y=target,                        #output data (targets) for training
                          size=i,                          #number of neurons in the hidden layer
                          learnFunc="Std_Backpropagation", #type of learning
                          learnFuncParams=c(0.01),         #paramenters of the learning function (eta)
                          maxit=50000)                     #maximum number of iterations
-                                                        
+                               
+            print(paste("C2",i))                         
             return(model)
         }
         
